ee161c9f299000bfa5489ce97132fb82332c03e9
Shlomi Fish <shlomif@shlomifish.org>
2022-10-19 11:54:47 +0300

    add text

--- Moral-GPS/generate_svg_meme.py
+++ Moral-GPS/generate_svg_meme.py
@@ -193,8 +193,10 @@ class TextSplit(inkex.EffectExtension):
             if split_type == "line":
                 textlines = []
                 textlines.append(TextLine(text="I AM NOT"))
-                textlines.append(TextLine(text="A MERE MORAL COMPASS"))
-                textlines.append(TextLine(text="I AM A MORAL GLOBAL"))
+                textlines.append(TextLine(text="A MERE MORAL"))
+                textlines.append(TextLine(text="COMPASS!"))
+                textlines.append(TextLine(text="I AM A MORAL"))
+                textlines.append(TextLine(text="GLOBAL"))
                 textlines.append(TextLine(text="POSITIONING SYSTEM"))
                 nodes = self.split_lines(elem, textlines)
                 # assert 0
